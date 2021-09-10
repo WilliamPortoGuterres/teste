37e1425788246e1bc9c64e32772e98b9c56f13de
William Porto Guterres <82295823+will4879@users.noreply.github.com>
2021-09-10 11:24:22 -0300

    Add files via upload

---
+++ controles/testaExistencia.php
@@ -0,0 +1,36 @@
+<?php
+  
+  
+ if(isset($_POST['cep1'])){$cep1=$_POST['cep1'];}
+ if(isset($_POST['cep2'])){$cep2=$_POST['cep2'];}
+ 
+ function testaCep($params){ 
+ 
+        $key = 'cafd025c295ee1228840cc45c58b954c';
+        $uri="https://www.cepaberto.com/api/v3/cep?cep=".$params;
+        $type_request='GET';
+        $request = curl_init();
+        curl_setopt ($request, CURLOPT_HTTPHEADER, array('Authorization:Token token='.$key)); 
+        curl_setopt ($request, CURLOPT_URL, $uri); 
+        curl_setopt ($request, CURLOPT_RETURNTRANSFER, 1); 
+        curl_setopt ($request, CURLOPT_CONNECTTIMEOUT, 5); 
+        curl_setopt ($request, CURLOPT_CUSTOMREQUEST, $type_request); 
+        $file_contents = curl_exec($request);
+        curl_close($request);
+        if($file_contents!='{}'){
+            
+            return "existe";
+        }else{
+
+            return"naoexiste";
+        }
+}
+ if(isset($cep1)){
+
+    echo testaCep($cep1);
+}
+if(isset($cep2)){
+
+    echo testaCep($cep2);
+}
+?>
