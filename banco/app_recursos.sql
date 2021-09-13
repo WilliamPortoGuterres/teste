831a2968d68d460ec03f407bdc54e57f714dcd97
William Porto Guterres <82295823+will4879@users.noreply.github.com>
2021-09-13 00:37:20 -0300

    Add files via upload

--- banco/app_recursos.sql
+++ banco/app_recursos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 13-Set-2021 às 04:09
+-- Tempo de geração: 13-Set-2021 às 05:34
 -- Versão do servidor: 10.4.21-MariaDB
 -- versão do PHP: 8.0.10
 
@@ -41,14 +41,8 @@ CREATE TABLE `dados` (
 --
 
 INSERT INTO `dados` (`id_dados`, `cepOrigem`, `cepDestino`, `distanciaCalculada`, `horaCadastro`, `horaAlteracao`) VALUES
-(22, 90230260, 91790131, '1 Km 0 metros', '2021-09-12 23:40:15', '0000-00-00 00:00:00'),
-(23, 91790425, 91790240, '0 Km 500 metros', '2021-09-13 01:29:02', '0000-00-00 00:00:00'),
-(24, 91793540, 91790427, '4 Km 700 metros', '2021-09-13 01:30:07', '2021-09-12 22:31:54'),
 (25, 91792150, 91790240, '1 Km 200 metros', '2021-09-13 01:30:40', '0000-00-00 00:00:00'),
-(26, 91790820, 91790425, '4 Km 200 metros', '2021-09-13 01:31:16', '0000-00-00 00:00:00'),
-(27, 91790425, 0, '0 Km 500 metros', '2021-09-13 01:48:02', '2021-09-12 22:48:57'),
-(28, 91790425, 0, '', '2021-09-13 01:49:14', '0000-00-00 00:00:00'),
-(29, 0, 0, '', '2021-09-13 01:49:24', '0000-00-00 00:00:00');
+(26, 91790820, 91790425, '4 Km 200 metros', '2021-09-13 01:31:16', '2021-09-13 00:32:23');
 
 --
 -- Índices para tabelas despejadas
@@ -68,7 +62,7 @@ ALTER TABLE `dados`
 -- AUTO_INCREMENT de tabela `dados`
 --
 ALTER TABLE `dados`
-  MODIFY `id_dados` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id_dados` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
